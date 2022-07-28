e62530109aec8e87104f4b6bc35f712fe10f7de2
SameerBandaru <sameerbandaru@SameerBandarus-MacBook-Pro.local>
2022-07-28 16:50:38 +0530

    added basic conf

---
+++ src/main/java/com/example/cidemo/CidemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidemoApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String getWelcomeMessage(){
+		return "First CI/CD demo with automatic build";
+	}
+
+
+}
